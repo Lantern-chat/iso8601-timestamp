b3d19833f527c155465c8f1e05cc8d59cf9d5bc9
novacrazy <novacrazy@gmail.com>
2025-10-07 13:35:17 -0500

    Fix test

--- tests/rkyv_08.rs
+++ tests/rkyv_08.rs
@@ -7,7 +7,11 @@ use rkyv_08::{from_bytes, rancor::Error, to_bytes};
 #[test]
 fn test_rkyv() {
     #[cfg(not(feature = "std"))]
-    let ts = Timestamp::from(datetime!(2024-09-01 12:32 PM).replace_millisecond(123).unwrap());
+    let ts = Timestamp::from(
+        iso8601_timestamp::datetime!(2024-09-01 12:32 PM)
+            .replace_millisecond(123)
+            .unwrap(),
+    );
 
     #[cfg(feature = "std")]
     let ts = Timestamp::from(Timestamp::now_utc().replace_millisecond(123).unwrap());
